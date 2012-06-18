eddd976b300614f1e9ad691676092b7668434ef7
Laurent <lc@centurion-projec.org>
2012-06-18 12:02:42 +0200

    Fix form.

--- forms/Model/Lifo.php
+++ forms/Model/Lifo.php
@@ -6,6 +6,7 @@ class Redirect_Form_Model_Lifo extends Centurion_Form_Model_Abstract
 
     public function __construct($options = array())
     {
+        $this->_exclude[] = 'proxy_model';
         $this->_model = Centurion_Db::getSingleton('redirect/lifo');
 
         $this->_elementLabels = array(
@@ -36,11 +37,17 @@ class Redirect_Form_Model_Lifo extends Centurion_Form_Model_Abstract
 
     public function setOldUrl($url) {
         $this->_oldUrl = $url;
+        $this->getElement('old_permalink')->setValue($url);
+        $this->getElement('old_permalink')->setAttrib('disabled', true);
+        $this->getElement('old_permalink')->setDescription(null);
     }
 
     public function _preSave()
     {
-        $this->_instance->old_permalink = $this->_oldUrl;
+        if (null !== $this->_oldUrl) {
+            $this->_instance->old_permalink = $this->_oldUrl;
+        }
+
         parent::_preSave();
     }
 }
